7c73efaaf98bb2ae4b1c9a04e466869f9512a221
Maruan Aguerdouh <maruan.mx@gmail.com>
2021-07-03 15:50:06 +0200

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,16 +118,13 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { :host => 'portfolio-maru.herokuapp.com' }  
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
-ActionMailer::Base.smtp_settings = {
-:user_name => ENV['SENDGRID_USERNAME'],
-:password => ENV['SENDGRID_PASSWORD'],
-:domain => 'heroku.com',
-:address => 'smtp.sendgrid.net',
-:port => 587,
-:authentication => :plain,
-:enable_starttls_auto => true
-}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'mydomain.herokuapp.com', #mydomain actually contains the realvalue
+    :authentication => :plain,
+  }
 end
